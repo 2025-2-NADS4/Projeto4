942a40e27bea2f52021022120beef7143e3a738a
Joao Brosselin <166879336+sfbrosselin@users.noreply.github.com>
2025-11-24 18:26:56 -0300

    Update app.py

--- src/Projeto_Cannoli/app/app.py
+++ src/Projeto_Cannoli/app/app.py
@@ -1,14 +1,13 @@
-# app/app.py (Configuração Mínima)
+
 import dash
 from dash import dcc, html
-from flask import Flask, session # Apenas 'session' do Flask
+from flask import Flask, session 
 
 # Inicializa o servidor Flask
 server = Flask(__name__)
 
-# --- CONFIGURAÇÃO DA SESSÃO ---
-# Apenas a chave secreta é necessária para as sessões nativas do Flask
-server.config["SECRET_KEY"] = "SUA_CHAVE_SECRETA_MUITO_SEGURA_AQUI" 
+
+server.config["SECRET_KEY"] = "" 
 # ------------------------------
 
 # Inicializa a aplicação Dash
@@ -21,11 +20,12 @@ app = dash.Dash(
 
 app.title = 'Cannoli Dashboard'
 
-# Layout principal: VAZIO para ser preenchido pelo callback
+
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
     html.Div(id='page-content')
 ])
 
-# Importa o controlador de rotas
+
+
 from . import index
